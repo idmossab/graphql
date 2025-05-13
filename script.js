bd9a28a808403ab684cb5dda4cb90f01842a5bce
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2025-05-13 11:29:53 +0100

    the jwt is extracted stored in local store

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-// import { login_user } from "./login"
+import { login_user } from "./login.js"
 
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", () => {
     let login_form = document.getElementById("login_form")
-    login_form.addEventListener("submit", async (e)=>{
+    login_form.addEventListener("submit", async (e) => {
         e.preventDefault()
         await login_user()
     })
+
+
+    let get_token = document.getElementById("get_token")
+    get_token.addEventListener("click", () => {
+        document.getElementById("display_token").textContent = get_token_from_local_storage()
+    })
+
+
 })
 
-async function login_user(){
-    let email = document.getElementById("email").value
-    let password = document.getElementById("password").value
-    let credentials = btoa(`${email}:${password}`)
-    console.log(credentials);
-    fetch("https://learn.zone01oujda.ma/api/auth/signin",{
-        method:'POST',
-        headers:{
-             'Authorization': `Basic ${credentials}`
-        }
-    })
-    .then(response => response.json())
-    .then(data => console.log("token: ", data))
-    .catch(err => console.error('Error:', err));
+// function to get the token from the loval storage:
+function get_token_from_local_storage() {
+    let token = localStorage.getItem('token')
+    return token
 }
