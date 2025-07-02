282b49eae85eb5b667d0151e843c2351c3325ce4
midbenke <idmossab@gmail.com>
2025-07-02 22:47:41 +0100

    make current level and audit ration beside

--- views/home.js
+++ views/home.js
@@ -18,36 +18,29 @@ export async function render_home_page() {
     container.innerHTML = `
         ${render_profile_sidebar()}
         <main class="main-content">
-         <div class="profile_details">
-             <h4>Current level</h4>
-            <div id="user_level">
-         
-            
-            </div>
-                 <div id="user_description">
-         
-            
-            </div>
+     <div class="profile_section_wrapper">
+    <!-- Left side: Current level -->
+    <div class="profile_details">
+        <h4>Current level</h4>
+        <div id="user_level"></div>
+        <div id="user_description"></div>
+    </div>
+
+    <!-- Right side: Audit ratio -->
+    <div class="profile_details">
+        <h4>Audit ratio</h4>
+        <div id="svg_ratio"></div>
+        <div id="given_taken">
+            <div id="definition"></div>
         </div>
-        <div class="profile_details">
-            <h4>Audit ratio</h4>
-            <div id="svg_ratio">
-         
-            
-            </div>
-            <div id="given_taken">
-            <div id="definition">
-            
-            </div>
-            </div>
-        </div>
-        
+    </div>
+</div>
         <div class="profile_details">
             <h4>Passed & failed progress</h4>
           
         </div>
     </main>
     `
-    render_header(); 
+    render_header();
     fetchUserData();
 }
