4a3163b9752012d674ee4725a5e787626aa29a1c
midbenke <idmossab@gmail.com>
2025-07-06 20:18:44 +0100

    change style login

--- components/login.js
+++ components/login.js
@@ -3,11 +3,32 @@ import { handle_login } from "../controllers/auth.js";
 export function render_login_form() {
     let container = document.getElementById("main_container");
     container.innerHTML = `
-        <form id="login_form">
-            <input id="identifier" type="text" placeholder="Enter your email or user name..." required>
-            <input id="password" type="password" placeholder="Enter your password" required>
-            <input type="submit" value="Submit">
-        </form>
+          <div class="login-wrapper">
+            <div class="login-container">
+                <h2 class="login-title">Welcome Back</h2>
+                <form id="login_form" class="login-form">
+                    <div class="input-group">
+                        <input 
+                            id="identifier" 
+                            type="text" 
+                            placeholder="Enter your email or username..." 
+                            required
+                            class="form-input"
+                        >
+                    </div>
+                    <div class="input-group">
+                        <input 
+                            id="password" 
+                            type="password" 
+                            placeholder="Enter your password" 
+                            required
+                            class="form-input"
+                        >
+                    </div>
+                    <button type="submit" class="submit-btn">Sign In</button>
+                </form>
+            </div>
+        </div>
     `;
     handle_login();
 }
