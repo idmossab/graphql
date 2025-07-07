6dce9448f2acfe2b7f9dfb9629df2c72375d135d
midbenke <idmossab@gmail.com>
2025-07-07 23:37:27 +0100

    handler error pasword or email

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,17 +3,14 @@ import { set_app_state } from "../utils/state.js";
 import { is_logged_in } from "../utils/state.js";
 
 export function handle_login() {
-  // إذا المستخدم داير login فعلاً (JWT صالح)، دخلو للصفحة الرئيسية
   if (is_logged_in()) {
     navigateTo("/");
     return;
   }
 
-  // جبد الفورم ديال تسجيل الدخول
   const login_form = document.getElementById("login_form");
   if (!login_form) return;
 
-  // ملي يدير submit
   login_form.addEventListener("submit", (e) => {
     e.preventDefault();
     login_user();
@@ -32,7 +29,6 @@ export function login_user() {
     return;
   }
 
-  // تحويل البيانات ل base64 (حسب Basic Auth)
   const credentials = btoa(`${identifier}:${password}`);
 
   fetch("https://learn.zone01oujda.ma/api/auth/signin", {
@@ -45,13 +41,13 @@ export function login_user() {
       if (!response.ok) {
         throw new Error("Invalid email or password");
       }
-      return response.text(); // JWT كيرجع كسلسلة
+      return response.text(); 
     })
     .then((token) => {
       if (!isValidJWT(token)) throw new Error("Invalid response");
       
-      set_app_state(token); // تخزين JWT فـ localStorage و app_state
-      navigateTo("/"); // انتقال للصفحة الرئيسية
+      set_app_state(token); 
+      navigateTo("/"); 
     })
     .catch((err) => {
       console.error("Login failed:", err);
